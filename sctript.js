f9b0c7d14016f0f0eda06c86272ac2dad1d24e69
Abhishek Yadav <118991273+abhiyadav05@users.noreply.github.com>
2025-02-26 01:37:38 +0530

    Add files via upload

--- sctript.js
+++ sctript.js
@@ -5,7 +5,7 @@
 function myFunction() {
     document.getElementById("cross-icon").style.display = "block",TransitionEvent=".5s ease";
     document.getElementById("menu-icon").style.display = "none"
-    document.getElementById("navbar").style.transform = "translateY(60px)";
+    document.getElementById("navbar").style.transform = "translateY(40px)";
     document.getElementById("navbar").style.top = "0%";
     
   }
@@ -16,8 +16,6 @@ function myFunction() {
     document.getElementById("navbar").style.transform = "translateY(-400px)";
   }
 
+  
+
  
-
-
-
-
